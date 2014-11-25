d6225ac06a32d625ae797742fc2676e5b05558d9
Hao Chen <chenhao@lavaripples.cn>
2014-11-25 12:33:03 +0800

    rm file

--- single_spider/ZOL-online_spider.js
+++ single_spider/ZOL-online_spider.js
@@ -17,6 +17,14 @@ http.get('http://detail.zol.com.cn/cell_phone_index/subcate57_list_1.html',funct
     console.error('can not request tmall');
 });
 
+http.get('http://detail.zol.com.cn/xhr_Header_WTrendEnter_proIds=p353416-p374241-p385410-p374242-p374499-p369691-p382520-p366588-p390206-p381220-p383462-p388980-p355153-p368904-p366586-p385937-p386683-p314780-p392016-p368216-p362464-p365757-p362774-p374502-p392292%5Ecallback=WtrendCallback.GetFollow.html?_=1416889263826',function(res){
+    res.on('data',function(data){
+        console.log(data.toString());
+    }).on('error',function(err){
+        console.log(err)
+    })
+});
+
 
 function getZOLData(data,encoding){
     //console.log(data.toString('gb2312'));
@@ -48,8 +56,23 @@ function getZOLData(data,encoding){
         if($.isEmptyObject(data)){
             return;
         }
-        setDataInRedis(data);
-        // console.log($('.category-menu-nav').find('.menu-nav').text());
+    
+
+        // 获取手机详情的列表
+        var product_list = $('.list-box').find('.list-item');
+        console.log(product_list.length)
+        var product_data_list=[];
+        for (var j=0;j<product_list.length;j++){
+            var product_info = {};
+            var image_url = $(product_list[j]).find('.pic-box').attr('data-rel');
+            product_info['image_url'] = image_url;
+            product_data_list.push(product_info);
+        } 
+        console.log(product_data_list)
+
+    
+        console.log(data)
+        // setDataInRedis(data);
     });
 }
 
