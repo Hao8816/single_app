cb439610b8457150b04a531cc490d565c38a779e
Hao8816 <chenhao8816+8816@gmail.com>
2014-11-29 00:13:36 +0800

    fix bug

--- single_spider/JD_goods_spider.js
+++ single_spider/JD_goods_spider.js
@@ -1,6 +1,5 @@
 // 定义搜索的一些关键词
 
-var search_key_words = ['手机','电脑'];
 var JD_download = require('./JD_image_spider');
 var http = require('http');
 var redis = require('redis');
@@ -8,74 +7,88 @@ var client =  redis.createClient();
 // 解决nodejs不支持gbk编码的问题
 var iconv = require('iconv-lite');
 
-// 请求Tmall的首页，爬取手机分类
-var url_list = [];
-// 构造搜索爬虫的url
-for (var i=0;i<search_key_words.length;i++){
-    //var url_encode_string = encodeURIComponent(search_key_words[i]);
-    url_list.push('http://search.jd.com/Search?keyword='+search_key_words[i]+'&enc=utf-8');
-    console.log(url_list);
-}
-
-
 
 // 定义buffer对象来存储返回的数据
 
-var buffer_list = [];
-
-http.get('http://list.jd.com/list.html?cat=9987,653,655',function(res){
-    res.on('data',function(data){
-        buffer_list.push(data);
-        console.log(data);
-    }).on('end',function(){
-        var new_buffer = Buffer.concat(buffer_list);
-        getJDGoodsData(new_buffer);
-        console.log('success')
-    }).on('error',function(err){
-        console.error('can not get response from JD');
-    })
-}).on('error',function(e){
-    console.error('can not request JD');
-});
-
-
-// 循环url 取得页面data
-/*for (var j=0;j<url_list.length;j++){
-    console.log(url_list[j]);
-    http.get(url_list[j],function(res){
+// 当前是显示手机页面的链接
+var basic_url = 'http://list.jd.com/list.html?cat=9987,653,655';
+for (var i=0;i<3;i++){
+    var buffer_list = [];
+    var goods_url = basic_url + '&page='+i+'&JL=6_0_0';
+    http.get(goods_url,function(res){
         res.on('data',function(data){
-            getJDGoodsData(data);
-            console.log(data);
+            buffer_list.push(data);
+            //console.log(data);
         }).on('end',function(){
+            var new_buffer = Buffer.concat(buffer_list);
+            getJDGoodsData(new_buffer);
             console.log('success')
-
         }).on('error',function(err){
             console.error('can not get response from JD');
         })
     }).on('error',function(e){
         console.error('can not request JD');
     });
-}*/
+}
 
 
 function getJDGoodsData(data) {
     //var decode_string = iconv.decode(data, 'GBK');
     var decode_string = data;
-    console.log(data.toString())
+    //console.log(data.toString())
     var env = require('jsdom').env;
     // 设置html环境
     env(decode_string.toString(), function (errors, window) {
         var $ = require('jquery')(window);
-        var goods_list = $('.list-h').find('li .lh-wrap');
+
+        // 获取当前页面的列表数据和图片链接
+        var goods_list = $('.list-h').find('li');
         console.log(goods_list.length);
         var image_url_list = [];
+        var goods_info_list = [];
+        var goods_sku_list = [];
         for (var i = 0; i < goods_list.length; i++) {
-            var url = $(goods_list[i]).find('.p-img img').attr('data-lazyload');
-            image_url_list.push(url);
+            var goods_info = {};
+            var goods_sku_id = $(goods_list[i]).attr('sku');
+            var goods_url = $(goods_list[i]).find('.lh-wrap .p-img a').attr('href');
+            var image_url = $(goods_list[i]).find('.lh-wrap .p-img img').attr('data-lazyload');
+            //var goods_desc = $(goods_list[i]).find('.p-name a').attr('title');
+            //console.log($(goods_list[i]).find('.p-price').html());
+            //console.log($(goods_list[i]).find('.p-name a').html())
+            var goods_desc = $(goods_list[i]).find('.lh-wrap .p-name').find('a').text();
+            // 这样的方式取得不了价格
+            var goods_price = $(goods_list[i]).find('.p-price').text();
+            if (typeof(image_url) == 'undefined'){
+                continue;
+            }
+            goods_info['goods_sku_id'] = goods_sku_id;
+            goods_info['goods_url'] = goods_url;
+            goods_info['image_url'] = image_url;
+            goods_info['goods_desc'] = goods_desc;
+            goods_info_list.push(goods_info);
+            image_url_list.push(image_url);
+            goods_sku_list.push(goods_sku_id);
         }
-        console.log(image_url_list);
-        cacheJDImageList(image_url_list);
-
+        //console.log(goods_info_list);
+        var script_list = $('script');
+        var area_id = '';
+        for (var s=0;s<script_list.length;s++){
+            if($(script_list[s]).text().indexOf('var params =')>=0){
+                var care_params = $(script_list[s]).text().split('var')[1];
+                care_params = care_params.replace('params = ','');
+                care_params = care_params.replace(';','');
+                var area_json = JSON.parse(care_params);
+                //console.log(area_json);
+                if (area_json.hasOwnProperty('area')){
+                    area_id = area_json['area'];
+                }
+            }
+        }
+        if(area_id != ''){
+            getJDGoodsPrice(goods_sku_list,area_id);
+        }
+        // 取得网页里面的script信息，取得获取价格的
+        //cacheJDImageList(image_url_list);
     });
 }
 
@@ -88,5 +101,17 @@ function cacheJDImageList(url_list){
             }
         })
     })
+}
 
+
+function getJDGoodsPrice(goods_sku_list,area_key){
+    console.log(area_key);
+    var area = area_key.replace(',','_');
+    http.get('http://p.3.cn/prices/mgets?skuIds=J_' + goods_sku_list.join(',J_') + '&type=1&area=' + area ,function(res){
+        res.on('data',function(data){
+            console.log(data.toString());
+        }).on('error',function(err){
+            console.log(err)
+        });
+    });
 }
