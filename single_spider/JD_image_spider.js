a7adc5f9828d02e3ce268ae1138bd0bec8a94b41
Hao8816 <chenhao8816+8816@gmail.com>
2014-11-28 21:33:12 +0800

    add module

--- single_spider/JD_image_spider.js
+++ single_spider/JD_image_spider.js
@@ -3,16 +3,11 @@
 var http = require('http');
 var fs = require('fs');
 
-
-
-
-
-
 function JD_image_download(file_url){
     // 取得文件名的后缀
     var file_path = file_url.split('/');
     var file_name = file_path[file_path.length-1];
-    var file = fs.createWriteStream(file_name);
+    var file = fs.createWriteStream('image/'+file_name);
     http.get(file_url,function(res){
         res.on('data',function(data){
             file.write(data);
